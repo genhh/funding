570c4341f4d0bbc09c8989b2ca4bb0dfef34f6a6
zhanghao2023 <ncepuzhzh@163.com>
2024-06-20 11:22:22 +0800

    second day

--- util/src/main/java/com/zh/funding/util/CrowdUtil.java
+++ util/src/main/java/com/zh/funding/util/CrowdUtil.java
@@ -4,10 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 
 
 public class CrowdUtil {
-    public boolean judgeRequestType(HttpServletRequest req){
+    public static boolean judgeRequestType(HttpServletRequest req){
         String reqHeader = req.getHeader("Accept");
         String xReqHeader = req.getHeader("X-Requested-with");
 
+        //if type == ajax return true
         return (reqHeader!=null && reqHeader.contains("application/json")) ||
                 (xReqHeader!=null && xReqHeader.equals("XMLHttpRequest"));
     }
