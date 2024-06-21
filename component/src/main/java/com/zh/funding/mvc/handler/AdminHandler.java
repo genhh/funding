1cf918137bd3ad75043eb6cfc7baab3af6fc5604
zhanghao2023 <ncepuzhzh@163.com>
2024-06-21 15:50:43 +0800

    log out

--- component/src/main/java/com/zh/funding/mvc/handler/AdminHandler.java
+++ component/src/main/java/com/zh/funding/mvc/handler/AdminHandler.java
@@ -15,11 +15,17 @@ public class AdminHandler {
     AdminService adminService;
 
     @RequestMapping("/admin/do/login.html")
-    public String doLogin(String loginAcct, String userPswd, HttpSession session){
+    public String doLogin(String loginAcct, String userPswd, HttpSession session) {
         //login check
         Admin admin = adminService.getAdminByLoginAcct(loginAcct, userPswd);
         //save admin in session
         session.setAttribute(CrowdConstant.ATTR_NAME_LOGIN_ADMIN, admin);
         return "redirect:/admin/to/main/page.html";
+    }
+
+    @RequestMapping("/admin/do/logout.html")
+    public String doLogout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/admin/to/login/page.html";
     }
 }
