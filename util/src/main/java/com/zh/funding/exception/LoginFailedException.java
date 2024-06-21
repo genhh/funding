da97f8d5bbcf23150466aec3cb235407405527aa
zhanghao2023 <ncepuzhzh@163.com>
2024-06-21 11:15:49 +0800

    Failed Login Exception extend

--- util/src/main/java/com/zh/funding/exception/LoginFailedException.java
+++ util/src/main/java/com/zh/funding/exception/LoginFailedException.java
@@ -1,7 +1,26 @@
 package com.zh.funding.exception;
 
 public class LoginFailedException extends RuntimeException{
-    public LoginFailedException(String message) {
+    private static final long serialVersionUID = 1L;
 
+    public LoginFailedException() {
+        super();
+    }
+
+    public LoginFailedException(String message, Throwable cause, boolean enableSuppression,
+                                boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LoginFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LoginFailedException(String message) {
+        super(message);
+    }
+
+    public LoginFailedException(Throwable cause) {
+        super(cause);
     }
 }
