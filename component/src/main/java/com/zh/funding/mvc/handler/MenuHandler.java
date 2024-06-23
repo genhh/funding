3745ca771bd6aebe56ab76accf9c0817f4cf6d52
zhanghao2023 <ncepuzhzh@163.com>
2024-06-23 18:37:32 +0800

    show menu tree struct

--- component/src/main/java/com/zh/funding/mvc/handler/MenuHandler.java
+++ component/src/main/java/com/zh/funding/mvc/handler/MenuHandler.java
@@ -1,11 +1,39 @@
 package com.zh.funding.mvc.handler;
 
+import com.zh.funding.entity.Menu;
 import com.zh.funding.service.api.MenuService;
+import com.zh.funding.util.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MenuHandler {
     @Autowired
     MenuService menuService;
+
+    @ResponseBody
+    @RequestMapping("/menu/get/whole/tree.json")
+    public ResultEntity<Menu> getMenuRoot() {
+        List<Menu> menuList = menuService.getall();
+        Map<Integer, Menu> menuMap = new HashMap<>();
+        Menu root = null; // root
+        for (Menu menu:menuList) {
+            menuMap.put(menu.getId(), menu);
+        }
+        for (Menu menu:menuList) {
+            if (menu.getPid() == null) {
+                root = menu;
+                continue;
+            }
+            Menu father = menuMap.get(menu.getPid());
+            father.getChildren().add(menu);
+        }
+        return ResultEntity.successWithData(root);
+    }
 }
