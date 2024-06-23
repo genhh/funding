2b54b84b0c630456f5c0a454ea0342f22e7a576e
zhanghao2023 <ncepuzhzh@163.com>
2024-06-23 12:40:52 +0800

    role show page func

--- creat.sql
+++ creat.sql
@@ -11,5 +11,8 @@ email varchar(255) NOT NULL, # 邮件地址
 create_time char(19), # 创建时间
 PRIMARY KEY (id)
 );
-INSERT INTO t_admin  VALUES(1,'admin','E10ADC3949BA59ABBE56E057F20F883E','testAdminer','222@examp.com','4-4');
+INSERT INTO t_admin  VALUES(1,'admin','E10ADC3949BA59ABBE56E057F20F883E','testAdminer','222@examp.com','4-4');#pswd:123456
+#后续的插入用户案例在com.zh.funding.test
 ALTER TABLE `t_admin` ADD UNIQUE INDEX (`login_acct`);
+DROP TABLE if EXISTS t_role;
+CREATE TABLE `t_role` ( `id` INT(11) NOT NULL AUTO_INCREMENT, `name` CHAR(100), PRIMARY KEY (`id`) );
