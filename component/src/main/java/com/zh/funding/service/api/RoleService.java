99dae9d9c354bee67d4e7ab035fee4ab30e5e78f
zhanghao2023 <ncepuzhzh@163.com>
2024-06-23 16:46:30 +0800

    role show page func

---
+++ component/src/main/java/com/zh/funding/service/api/RoleService.java
@@ -0,0 +1,8 @@
+package com.zh.funding.service.api;
+
+import com.github.pagehelper.PageInfo;
+import com.zh.funding.entity.Role;
+
+public interface RoleService {
+    PageInfo<Role> getPageInfo(Integer pageNum, Integer pageSize, String keyword);
+}
