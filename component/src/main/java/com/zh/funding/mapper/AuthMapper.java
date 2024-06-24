2aca801473895f7300c9147ae283c4f4495e7fcb
zhanghao2023 <ncepuzhzh@163.com>
2024-06-24 20:01:27 +0800

    give auth for role

---
+++ component/src/main/java/com/zh/funding/mapper/AuthMapper.java
@@ -0,0 +1,36 @@
+package com.zh.funding.mapper;
+
+import com.zh.funding.entity.Auth;
+import com.zh.funding.entity.AuthExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AuthMapper {
+    int countByExample(AuthExample example);
+
+    int deleteByExample(AuthExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Auth record);
+
+    int insertSelective(Auth record);
+
+    List<Auth> selectByExample(AuthExample example);
+
+    Auth selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByExample(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByPrimaryKeySelective(Auth record);
+
+    int updateByPrimaryKey(Auth record);
+
+    List<Integer> selectAssignedAuthIdByRoleId(Integer roleId);
+
+    void deleteOldRelationship(Integer roleId);
+
+    void insertNewRelationship(Integer roleId, List<Integer> authIdList);
+}
