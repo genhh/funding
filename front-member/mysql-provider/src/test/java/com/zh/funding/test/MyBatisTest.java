f6609817147574d1838c713d99e549149948ae5d
zhanghao2023 <ncepuzhzh@163.com>
2024-06-28 18:10:21 +0800

    bug fixing

--- front-member/mysql-provider/src/test/java/com/zh/funding/test/MyBatisTest.java
+++ front-member/mysql-provider/src/test/java/com/zh/funding/test/MyBatisTest.java
@@ -3,17 +3,19 @@ package com.zh.funding.test;
 import com.zh.funding.entity.po.MemberPO;
 import com.zh.funding.mapper.MemberPOMapper;
 import com.zh.funding.util.CrowdUtil;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyBatisTest {
     @Autowired
@@ -41,6 +43,6 @@ public class MyBatisTest {
 
         Connection connection = dataSource.getConnection();
 
-        logger.debug(connection.toString());
+        //logger.debug(connection.toString());
     }
 }
