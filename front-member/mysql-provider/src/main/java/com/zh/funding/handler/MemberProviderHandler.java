57275839309943d7f62c2d78be7bebad7d686b21
zhanghao2023 <ncepuzhzh@163.com>
2024-06-29 15:51:51 +0800

    redis handler func and fix mysqlProviderHandler.java with @restController

--- front-member/mysql-provider/src/main/java/com/zh/funding/handler/MemberProviderHandler.java
+++ front-member/mysql-provider/src/main/java/com/zh/funding/handler/MemberProviderHandler.java
@@ -6,7 +6,9 @@ import com.zh.funding.util.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MemberProviderHandler {
     @Autowired
     MemberService memberService;
---
+++ front-member/redis-provider/src/main/java/com/zh/funding/RedisMainClass.java
@@ -0,0 +1,11 @@
+package com.zh.funding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMainClass.class,args);
+    }
+}
