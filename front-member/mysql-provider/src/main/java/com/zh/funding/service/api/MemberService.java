2daf2553b6af4833d05de0db1d1582b7ecc82168
zhanghao2023 <ncepuzhzh@163.com>
2024-07-01 21:13:30 +0800

    member login func ing:phone code send

--- front-member/mysql-provider/src/main/java/com/zh/funding/service/api/MemberService.java
+++ front-member/mysql-provider/src/main/java/com/zh/funding/service/api/MemberService.java
@@ -1,7 +1,9 @@
 package com.zh.funding.service.api;
 
 import com.zh.funding.frontentity.po.MemberPO;
+import com.zh.funding.util.ResultEntity;
 
 public interface MemberService {
     MemberPO getMemberPOByLoginAcct(String loginacct);
+    ResultEntity<String> saveMember(MemberPO memberPO);
 }
