c336ca831060f6e917072bf591ed940171bade21
zhanghao2023 <ncepuzhzh@163.com>
2024-07-02 17:02:19 +0800

    bug fixing

--- front-member/authentication-consumer/src/main/java/com/zh/funding/AuthMainClass.java
+++ front-member/authentication-consumer/src/main/java/com/zh/funding/AuthMainClass.java
@@ -3,7 +3,9 @@ package com.zh.funding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients //不加会报错
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthMainClass {
--- front-member/mysql-provider/src/main/java/com/zh/funding/handler/MemberProviderHandler.java
+++ front-member/mysql-provider/src/main/java/com/zh/funding/handler/MemberProviderHandler.java
@@ -4,6 +4,7 @@ import com.zh.funding.frontentity.po.MemberPO;
 import com.zh.funding.service.api.MemberService;
 import com.zh.funding.util.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,28 @@ public class MemberProviderHandler {
             e.printStackTrace();
 
             // 3.如果捕获到异常则返回失败的结果
+            return ResultEntity.failed(e.getMessage());
+        }
+
+    }
+
+    @RequestMapping("/save/member/remote")
+    public ResultEntity<String> saveMember(
+            @RequestBody MemberPO memberPO) {
+
+
+        try {
+
+            memberService.saveMember(memberPO);
+
+            return ResultEntity.successWithoutData();
+
+        }catch(Exception e) {
+
+            if(e instanceof DuplicateKeyException) {
+                return ResultEntity.failed(CrowdConstant.MESSAGE_LOGIN_ACCT_ALREADY_IN_USE);
+            }
+
             return ResultEntity.failed(e.getMessage());
         }
 
