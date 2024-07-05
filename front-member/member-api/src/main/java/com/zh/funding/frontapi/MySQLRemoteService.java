6056571bb5845b54bcd1e691aba4916f1d17a0d3
zhanghao2023 <ncepuzhzh@163.com>
2024-07-05 16:00:11 +0800

    remote interface add

--- front-member/member-api/src/main/java/com/zh/funding/frontapi/MySQLRemoteService.java
+++ front-member/member-api/src/main/java/com/zh/funding/frontapi/MySQLRemoteService.java
@@ -1,12 +1,17 @@
 package com.zh.funding.frontapi;
 
 import com.zh.funding.frontentity.po.MemberPO;
+import com.zh.funding.frontentity.vo.DetailProjectVO;
+import com.zh.funding.frontentity.vo.PortalTypeVO;
 import com.zh.funding.frontentity.vo.ProjectVO;
 import com.zh.funding.util.ResultEntity;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("crowd-mysql")
 public interface MySQLRemoteService {
@@ -17,4 +22,9 @@ public interface MySQLRemoteService {
     public ResultEntity<String> saveMember(@RequestBody MemberPO memberPO);
     @RequestMapping("/save/project/vo/remote")
     ResultEntity<String> saveProjectVORemote(@RequestBody ProjectVO projectVO, @RequestParam("memberId") Integer memberId);
+    @RequestMapping("/get/portal/type/project/data/remote")
+    public ResultEntity<List<PortalTypeVO>> getPortalTypeProjectDataRemote();
+
+    @RequestMapping("/get/project/detail/remote/{projectId}")
+    public ResultEntity<DetailProjectVO> getDetailProjectVORemote(@PathVariable("projectId") Integer projectId);
 }
